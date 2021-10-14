89d6c3e136e01af6be0385bc648906600513809e
daliayan <daliayan5@gmail.com>
2021-10-13 20:42:54 -0400

    work on funds + rendering data + styling

--- frontend/src/components/FundCard.js
+++ frontend/src/components/FundCard.js
@@ -7,22 +7,28 @@ const FundCard =props => {
         <div id="dollar-bills">
             
                 <p id="one-bill"> $1
-                <img></img>
+                {/* <img></img> */}
                 </p>
 
                 <p id="five-bill"> $5
+                {/* <img></img> */}
                 </p>
             
                 <p id="ten-bill"> $10
+                {/* <img></img> */}
                 </p>
 
                 <p id="twenty-bill"> $20
+                {/* <img></img> */}
                 </p>
 
                 <p id="fifty-bill"> $50
+                {/* <img></img> */}
                 </p>
 
-                <p id="hundred-bill"> $100 {props.value}
+                <p id="hundred-bill"> $100
+                {/* <img></img> */} 
+                {/* {props.value} */}
                 </p>
         </div>
     )
