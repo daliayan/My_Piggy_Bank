4c7ef4c82682f69ce039efb2c8d3510f798865ab
daliayan <daliayan5@gmail.com>
2021-10-15 22:08:54 -0400

    Connected 16770 // need to assign values + connect + submit forms

--- frontend/src/components/FundCard.js
+++ frontend/src/components/FundCard.js
@@ -1,12 +1,12 @@
 import React from 'react';
 // import {connect} from "react-redux";
 import piggy from '../images/piggy.jpg';
-// import one from '../images/money/one.jpg';
-// import five from '../images/money/five.jpg';
-// import ten from '../images/money/ten.jpg';
-// import twenty from '../images/money/twenty.jpg';
-// import fifty from '../images/money/fifty.jpg';
-// import hundred from '../images/money/hundred.jpg';
+import one from '../images/money/one.jpg';
+import five from '../images/money/five.jpg';
+import ten from '../images/money/ten.jpg';
+import twenty from '../images/money/twenty.jpg';
+import fifty from '../images/money/fifty.jpg';
+import hundred from '../images/money/hundred.jpg';
 // assiging the $$ their values
 
 const FundCard = (props) => {
@@ -15,32 +15,35 @@ const FundCard = (props) => {
         return (
         <div>
                 <img src={piggy} className='piggy-img' alt=''></img>
+
            <div className="dollars">
-                <p id="one-bill"> $1
-                {/* <img src={one} alt=''></img> */}
-                </p>
+                   <div className='row'>
+                        <p id="one-bill"> $1
+                                <img src={one} alt=''></img>
+                        </p>
 
-                <p id="five-bill"> $5
-                {/* <img src={five} alt=''></img> */}
-                </p>
-            
-                <p id="ten-bill"> $10
-                {/* <img src={ten} alt=''></img> */}
-                </p>
+                        <p id="five-bill"> $5
+                        <img src={five} alt=''></img>
+                        </p>
+                
+                        <p id="ten-bill"> $10
+                        <img src={ten} alt=''></img>
+                        </p>
 
-                <p id="twenty-bill"> $20
-                {/* <img src={twenty} alt=''></img> */}
-                </p>
+                        <p id="twenty-bill"> $20
+                        <img src={twenty} alt=''></img>
+                        </p>
 
-                <p id="fifty-bill"> $50
-                {/* <img src={fifty} alt=''></img> */}
-                </p>
+                        <p id="fifty-bill"> $50
+                        <img src={fifty} alt=''></img>
+                        </p>
 
-                <p id="hundred-bill"> $100
-                {/* <img src={hundred} alt=''></img> */} 
-                {/* {props.value} */}
-                </p>
-        </div> 
+                        <p id="hundred-bill"> $100
+                        <img src={hundred} alt=''></img> 
+                        {/* {props.value} */}
+                        </p>
+                     </div>
+                </div> 
         </div>
     )
 }
