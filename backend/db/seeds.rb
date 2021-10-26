f615934fd4c86687a01480527cb8425a24668628
daliayan <daliayan5@gmail.com>
2021-10-26 14:56:23 -0400

    dropped tables - reseeded - need to implement bio page / redux

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,4 +1,4 @@
-bank_one = Bank.create(name: 'Polly', gender: 'girl', fund: 100)
-bank_two = Bank.create(name: 'Joe', gender: 'boy', fund: 50)
-bank_three = Bank.create(name: 'Annie', gender: 'girl', fund: 200)
-bank_four = Bank.create(name: 'Jack', gender: 'boy', fund: 1500)
+bank_one = Bank.create(name: 'Polly', gender: 'GIRL', fund: 100)
+bank_two = Bank.create(name: 'Joe', gender: 'BOY', fund: 50)
+bank_three = Bank.create(name: 'Annie', gender: 'GIRL', fund: 200)
+bank_four = Bank.create(name: 'Jack', gender: 'BOY', fund: 1500)
