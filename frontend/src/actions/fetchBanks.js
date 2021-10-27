ec27190d521bb483f7c76c2efcf6a6afb98fcfd0
daliayan <daliayan5@gmail.com>
2021-10-26 21:28:53 -0400

    Fetching from backend using redux/dispatch

--- frontend/src/actions/fetchBanks.js
+++ frontend/src/actions/fetchBanks.js
@@ -1,14 +1,3 @@
-// export const fetchBanks= () => {
-//     // return (dispatch) => {
-//     //     dispatch({ type: 'ADD_BANKS' });
-//         return (
-//         fetch('http://localhost:3000/banks') 
-//         .then(resp => resp.json())
-//         // .then(banks => console.log(banks))
-//         //.then(banks => dispatch({type: 'ADD_BANKS', banks: banks}))
-//         );
-// }
-
 export function fetchBanks() {
      return (dispatch) => {
         dispatch({ type: 'ADD_BANKS' });
