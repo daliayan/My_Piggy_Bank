04e598c548e1762458e2d27b2e39d7ce9bf5abf6
daliayan <daliayan5@gmail.com>
2021-11-12 18:53:02 -0500

    Adding functionality

--- frontend/src/components/BankList.js
+++ frontend/src/components/BankList.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import {fetchBanks} from '../actions/fetchBanks';
 // import LikeButton from '../components/LikeButton'
+// import AdderButton from '../components/AdderButton'
 
 class BankList extends Component {
 
@@ -42,6 +43,7 @@ class BankList extends Component {
                 DELETE
             </button>
             {/* <LikeButton/> */}
+            {/* <AdderButton /> */}
         </ol>
         </div>)
     }
